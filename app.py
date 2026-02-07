a7f79f550040c26a3c9ca0911df572482532195e
cry0ne-ops <cyronejohnsarmiento@gmail.com>
2026-02-07 09:41:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -279,7 +279,7 @@ with col_input:
 
 # --- Column 2: Predictions & Metrics ---
 with col_pred:
-    st.subheader("📊 Prediction ")
+    st.subheader("📊 Predictions & Metrics")
     if "predictions" in st.session_state:
         preds = st.session_state["predictions"]
         pred_col1, pred_col2, pred_col3 = st.columns(3)
